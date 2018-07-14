853ebb1748147c9c959cab5acda0445b84b6cc84
Charly <charly.martin.1207@gmail.com>
2018-07-14 19:19:28 +0200

    index.jsx ready

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import '../assets/stylesheets/application.scss';
+
+// const Hello = props => <div>Hello, {props.name}</div>;
+
+// const root = document.getElementById('root');
+// if (root) {
+//   ReactDOM.render(<Hello name="World" />, root);
+// }
+
+// Copied from solution
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-const Hello = props => <div>Hello, {props.name}</div>;
+// import App from './components/app';
 
 const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
+// ReactDOM.render(<App />, root);
